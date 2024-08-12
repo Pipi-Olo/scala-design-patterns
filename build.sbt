e2dc9c0f33ac2dc735b835b4fe4896a1eecb9379
pipiolo <pipiolo1024@gmail.com>
2024-08-12 22:41:17 +0900

    add pipeline pattern

--- build.sbt
+++ build.sbt
@@ -1,5 +1,9 @@
-ThisBuild / version := "0.3.0-SNAPSHOT"
+import sbt.Keys.libraryDependencies
+
+ThisBuild / version := "0.4.0-SNAPSHOT"
 ThisBuild / scalaVersion := "2.13.14"
+
+val scalaXmlVersion = "2.2.0"
 
 lazy val root = (project in file("."))
   .settings(
@@ -16,4 +20,13 @@ lazy val observerPattern = (project in file("behavior-pattern/observer"))
 lazy val factoryMethodPattern = (project in file("creational-pattern/factory-method"))
   .settings(
     name := "scala-design-patterns-creational-factory-method-pattern"
+  )
+
+// Structure Pattern
+lazy val pipelinePattern = (project in file("structure-pattern/pipeline"))
+  .settings(
+    name := "scala-design-patterns-structure-pipeline-pattern",
+    libraryDependencies ++= Seq(
+      "org.scala-lang.modules" %% "scala-xml" % scalaXmlVersion
+    )
   )
---
+++ structure-pattern/pipeline/src/main/scala/com/pipiolo/structure/pipeline/PeoplePipeline.scala
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2024 PipiOlo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pipiolo.structure.pipeline
+
+import java.nio.file.Path
+
+class PeoplePipeline {
+  private val pathToXml = new PathToXmlConverter().convert
+  private val xmlToPeople = new XmlToPeopleConverter().convert
+
+  def process: Path => Seq[Person] = {
+    pathToXml.andThen(xmlToPeople)
+  }
+}
